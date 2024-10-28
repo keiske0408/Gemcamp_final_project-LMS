49cc16792d7f382e32958b1acdc535361ee74dce
keiske0408 <kieske0408@gmail.com>
2024-10-28 14:06:22 +0800

    update: application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     if current_user.admin?
-      admin_users_path
+      root_path
     else
       client_dashboard_path
     end
