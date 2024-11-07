26a03cc6d636a59395cc96f8c9c94c07fa2ca84d
keiske0408 <kieske0408@gmail.com>
2024-11-07 17:24:16 +0800

    added: Design own API[feature 5]

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,27 @@ Rails.application.routes.draw do
       resource :me, controller: :me, only: [:show]
     end
   end
+
+  namespace :api do
+    namespace :v1 do
+      resources :regions, only: %i[index show], defaults: { format: :json } do
+        resources :provinces, only: :index, defaults: { format: :json }
+      end
+
+      resources :provinces, only: %i[index show], defaults: { format: :json } do
+        resources :cities, only: :index, defaults: { format: :json }
+      end
+
+      resources :cities, only: %i[index show], defaults: { format: :json } do
+        resources :barangays, only: :index, defaults: { format: :json }
+      end
+
+      resources :barangays, only: %i[index show], defaults: { format: :json }
+
+    end
+  end
 end
+
+
 
 
