17263f66bab8766b8a9cdab2f9062517c2c47592
keiske0408 <kieske0408@gmail.com>
2024-11-11 13:22:07 +0800

    updated: user model for counting children [feature 8]

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   mount_uploader :image, ImageUploader
+  belongs_to :parent, class_name: 'User', optional: true, counter_cache: :children_members
+  has_many :children_members, class_name: 'User', foreign_key: :parent_id, counter_cache: :children_members
+  after_create :increment_parent_children_count, if: -> { parent.present? }
 
   enum genre: { client: 0, admin: 1 }
   devise :database_authenticatable, :registerable,
@@ -20,6 +23,10 @@ class User < ApplicationRecord
   has_many :locations, class_name: 'Location'
 
   private
+
+  def increment_parent_children_count
+    parent.increment!(:children_members)
+  end
 
   def location_limit
     errors.add(:locations, "limit of 5 addresses per user") if locations.size > 5
