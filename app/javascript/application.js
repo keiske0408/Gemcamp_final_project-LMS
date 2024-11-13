9002960b0b75bd8c8457e51849a1a90c39ef6275
keiske0408 <kieske0408@gmail.com>
2024-11-13 19:24:33 +0800

    Update application.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,7 +2,7 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
-// Turbo.session.drive = false
+Turbo.session.drive = false
 import jQuery from "jquery"
 
 window.jQuery = jQuery
