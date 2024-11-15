9b71fe3449d74cead809906bc3f7a2dc95d3c9e9
keiske0408 <kieske0408@gmail.com>
2024-11-16 01:40:16 +0800

    update categories and base controller

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,6 +1,5 @@
-class Admin::CategoriesController < ApplicationController
+class Admin::CategoriesController < Admin::BaseController
   before_action :set_category, only: %i[show edit update destroy restore]
-  layout 'admin'
 
   def index
     @categories = Category.all
