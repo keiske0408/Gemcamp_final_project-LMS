5416f64a9b9ec83f074822738e6098ce5b0c3fca
keiske0408 <kieske0408@gmail.com>
2024-11-25 17:30:28 +0800

    updated: lottery views and controller

--- app/controllers/client/lottery_controller.rb
+++ app/controllers/client/lottery_controller.rb
@@ -2,7 +2,6 @@ class Client::LotteryController < ApplicationController
   before_action :authenticate_client!, only: [:buy_tickets]
   def index
     @categories = Category.all
-    Rails.logger.debug "Categories: #{@categories.inspect}"
 
     @selected_category = params[:category]
     @items = Item.where(status: 1, state: 'starting')
@@ -16,34 +15,34 @@ class Client::LotteryController < ApplicationController
     @paginated_items = @items.page(params[:page]).per(3)
   end
 
-    def show
-      @item = Item.find(params[:id])
-      @total_tickets = @item.tickets.count # Total tickets purchased
-      @minimum_tickets = @item.minimum_tickets # Minimum tickets required
-      # Ensure only items with state 'starting' are viewable
-      if @item.state != 'starting'
-        redirect_to client_lottery_index_path, alert: 'This item is not available.'
-        return
-      end
-
-      # Show the client's own tickets for this item
-      if current_client
-        @user_tickets = @item.tickets.where(user: current_client)
-      else
-        @user_tickets = []
-      end
+  def show
+    @item = Item.find(params[:id])
+    @total_tickets = @item.tickets.count # Total tickets purchased
+    @minimum_tickets = @item.minimum_tickets # Minimum tickets required
+    # Ensure only items with state 'starting' are viewable
+    if @item.state != 'starting'
+      redirect_to client_lottery_index_path, alert: 'This item is not available.'
+      return
     end
 
-    def buy_tickets
-      @item = Item.find(params[:id])
-      quantity = params[:quantity].to_i
+    # Show the client's own tickets for this item
+    if current_client
+      @user_tickets = @item.tickets.where(user: current_client)
+    else
+      @user_tickets = []
+    end
+  end
 
-      # Check if user has enough coins
-      if current_client.coins < quantity
-        redirect_to client_lottery_path(@item), alert: 'Not enough coins to buy tickets.'
-        return
-      end
+  def buy_tickets
+    @item = Item.find(params[:id])
+    quantity = params[:quantity].to_i
 
+    # Check if user has enough coins
+    if current_client.coins < quantity
+      flash[:alert] = 'Not enough coins to buy tickets.'
+      redirect_to client_lottery_path(@item)
+      return
+    else
       # Create tickets and deduct coins
       Ticket.transaction do
         quantity.times do
@@ -56,17 +55,17 @@ class Client::LotteryController < ApplicationController
             coins: 1
           )
         end
-
-        current_client.update!(coins: current_client.coins - quantity)
+        current_client.decrement!(:coins, quantity)
       end
-
-      redirect_to client_lottery_path(@item), notice: "Successfully bought #{quantity} ticket(s)."
-    end
-
-    private
-
-    def generate_serial_number(item)
-      "#{item.id}-#{SecureRandom.hex(4)}"
+      flash[:notice] = "Successfully bought #{quantity} ticket(s)."
+      redirect_to client_lottery_path(@item)
     end
   end
+
+  private
+
+  def generate_serial_number(item)
+    "#{item.id}-#{SecureRandom.hex(4)}"
+  end
+end
 
