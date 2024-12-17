539493dedb69b4a0a7b0f633a33f78ac58e9f423
keiske0408 <kieske0408@gmail.com>
2024-12-18 02:43:03 +0800

    add jquery location controller

--- app/javascript/controllers/location_controller.js
+++ app/javascript/controllers/location_controller.js
@@ -1,4 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
+import $ from "jquery"
+
 
 export default class extends Controller {
     static targets = ['selectedRegionId', 'selectProvinceId', 'selectCityId', 'selectBarangayId']
